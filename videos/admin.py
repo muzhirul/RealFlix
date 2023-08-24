6fecf14982742775b9011141c9c62139979ab88c
Md. Muzhirul Islam <43285@wgbd.com>
2023-08-24 11:54:34 +0600

    ci:add model and admin

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,41 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from .models import Video, VideoPublishedProxy, VideoAllProxy
 
 # Register your models here.
+
+
+class VideoAllProxyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'state', 'video_id', 'is_published']
+    list_filter = ['video_id', 'state']
+    search_fields = ['title', 'video_id']
+    readonly_fields = ['id', 'is_published']
+
+    class Meta:
+        model = VideoAllProxy
+
+    # def published(self, obj, *args, **kwargs):
+    #     return obj.active
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    list_filter = ['video_id']
+    search_fields = ['title', 'video_id']
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    ''' 
+    Only show the active videos list
+    '''
+
+    def get_queryset(self, request):
+        return Video.objects.filter(active=True)
+
+
+admin.site.register(VideoAllProxy, VideoAllProxyAdmin)
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
+# admin.site.register(VideoAllProxy)
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,41 @@
+from re import M
+from tabnanny import verbose
+from weakref import proxy
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    class VideoStateOptions(models.TextChoices):
+        # CONSTANT = DB_VALUE, USER_DISPLAY_VALUE
+        PUSLISH = 'PU', 'Publish'
+        DRAFT = 'DR', 'Draft'
+        # UNLISTED = 'UN', 'Unlisted'
+        # PRIVATE = 'PR', 'Private'
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
+    video_id = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    state = models.CharField(
+        max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+
+    @property
+    def is_published(self):
+        return self.active
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Videos'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Videos'
+        verbose_name_plural = 'Published Videos'
