fe97c780fe7a1736a66b3b2d1d5313a13a54c3cd
Md. Muzhirul Islam <43285@wgbd.com>
2023-08-26 12:24:48 +0600

    feat: separete type of movie using proxy method

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,5 +1,75 @@
 from django.contrib import admin
-from .models import Playlist
+from .models import MovieProxy, Playlist, PlaylistItem, TVShowProxy, TVShowSeasionProxy
 # Register your models here.
 
-admin.site.register(Playlist)
+
+class MovieProxyAdmiin(admin.ModelAdmin):
+    list_display = ['title']
+    fields = ['title', 'description', 'state', 'video', 'slug']
+
+    class Meta:
+        model = MovieProxy
+
+    def get_queryset(self, request):
+        return MovieProxy.objects.all()
+
+
+admin.site.register(MovieProxy, MovieProxyAdmiin)
+
+
+class SeasonEpisodeInline(admin.TabularInline):
+    model = PlaylistItem
+    extra = 0
+
+
+class TVShowSeasonProxyAdmin(admin.ModelAdmin):
+    inlines = [SeasonEpisodeInline]
+    list_display = ['title', 'parent']
+
+    class Meta:
+        model = TVShowSeasionProxy
+
+    def get_queryset(self, request):
+        return TVShowSeasionProxy.objects.all()
+
+
+admin.site.register(TVShowSeasionProxy, TVShowSeasonProxyAdmin)
+
+
+class TVShowSeasonProxyInline(admin.TabularInline):
+    model = TVShowSeasionProxy
+    extra = 0
+    fields = ['order', 'title', 'state']
+
+
+class TVShowProxyAdmin(admin.ModelAdmin):
+    inlines = [TVShowSeasonProxyInline]
+    fields = ['title', 'description', 'state', 'video', 'slug']
+    list_display = ['title']
+
+    class Meta:
+        model = TVShowProxy
+
+    def get_queryset(self, request):
+        return TVShowProxy.objects.all()
+
+
+admin.site.register(TVShowProxy, TVShowProxyAdmin)
+
+
+class PlaylistItemInline(admin.TabularInline):
+    model = PlaylistItem
+    extra = 0
+
+
+class PlaylistAdmin(admin.ModelAdmin):
+    inlines = [PlaylistItemInline]
+
+    class Meta:
+        model = Playlist
+
+    def get_queryset(self, request):
+        return Playlist.objects.filter(type=Playlist.PlaylistTypeChoices.PLAYLIST)
+
+
+admin.site.register(Playlist, PlaylistAdmin)
--- playlists/models.py
+++ playlists/models.py
@@ -1,5 +1,5 @@
 
-from tkinter import N
+from weakref import proxy
 from django.utils import timezone
 from django.db import models
 from django.db.models.signals import pre_save
@@ -28,13 +28,24 @@ class PlaylistManager(models.Manager):
 
 
 class Playlist(models.Model):
+    class PlaylistTypeChoices(models.TextChoices):
+        MOVIE = "MOV", "Movie"
+        SHOW = "TVS", "TV Show"
+        SEASON = "SEA", "Season"
+        PLAYLIST = "PLY", "Playlist"
+
+    parent = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.SET_NULL)
+    order = models.IntegerField(default=1)
     title = models.CharField(max_length=255)
+    type = models.CharField(
+        max_length=3, choices=PlaylistTypeChoices.choices, default=PlaylistTypeChoices.PLAYLIST)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video = models.ForeignKey(
         Video, null=True, blank=True, on_delete=models.SET_NULL)
     videos = models.ManyToManyField(
-        Video, blank=True, related_name='playlist_item')
+        Video, blank=True, related_name='playlist_item', through='PlaylistItem')
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -45,6 +56,9 @@ class Playlist(models.Model):
 
     objects = PlaylistManager()
 
+    def __str__(self):
+        return self.title
+
     @property
     def is_published(self):
         return self.active
@@ -52,3 +66,76 @@ class Playlist(models.Model):
 
 pre_save.connect(publish_state_pre_save, sender=Playlist)
 pre_save.connect(slugify_pre_save, sender=Playlist)
+
+
+class MovieProxyManager(PlaylistManager):
+    def all(self):
+        return self.get_queryset().filter(type=Playlist.PlaylistTypeChoices.MOVIE)
+
+
+class MovieProxy(Playlist):
+
+    objects = MovieProxyManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.type = Playlist.PlaylistTypeChoices.MOVIE
+        super().save(*args, **kwargs)
+
+
+class TVShowProxyManager(PlaylistManager):
+    def all(self):
+        return self.get_queryset().filter(parent__isnull=True, type=Playlist.PlaylistTypeChoices.SHOW)
+
+
+class TVShowProxy(Playlist):
+
+    objects = TVShowProxyManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'TV Show'
+        verbose_name_plural = 'TV Shows'
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.type = Playlist.PlaylistTypeChoices.SHOW
+        super().save(*args, **kwargs)
+
+
+class TVShowSeasonProxyManager(PlaylistManager):
+    def all(self):
+        return self.get_queryset().filter(parent__isnull=False, type=Playlist.PlaylistTypeChoices.SEASON)
+
+
+class TVShowSeasionProxy(Playlist):
+
+    objects = TVShowSeasonProxyManager()
+
+    class Meta:
+        verbose_name = 'Season'
+        verbose_name_plural = 'Seasons'
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.type = Playlist.PlaylistTypeChoices.SEASON
+        super().save(*args, **kwargs)
+
+
+class PlaylistItem(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    order = models.IntegerField(default=1)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['order', '-timestamp']
